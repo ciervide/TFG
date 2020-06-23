37cfe391b396b67f918b84c372204e8abcf7288f
ciervide <ciervide@users.noreply.github.com>
2020-06-23 12:00:43 +0200

    Update Nmea.ino
    Nmea update to take into account possible errors when measuring

--- Arduino/Sensors/Nmea/Nmea.ino
+++ Arduino/Sensors/Nmea/Nmea.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <Gaussian.h>
 
 #define rxPin 4
 #define txPin 5
@@ -8,17 +9,22 @@ String str = "";
 float f_lat = 180, f_lon = 180, dist = 0, max_dist = 0, num_measures = 0;
 float EARTH_RADIUS = 6371e3;
 
+Gaussian gen;
+float MEAN = 0.2, VAR = 0.4;
+
 unsigned long previousMillis = 0;
 
 void setup() {
-  
+  // Setup the neccesary items to use the GPS antenna
   pinMode(rxPin, INPUT); pinMode(txPin, OUTPUT);
   gps.begin(9600); gps.flush();
   Serial.begin(9600);
-  
   if (gps.isListening()) {
     Serial.print("Reading serial on pin "); Serial.println(rxPin);
   }
+
+  // Initialize the normal distribution number generator
+  gen = Gaussian(MEAN, VAR);
 }
 
 void loop() {
@@ -65,35 +71,45 @@ void loop() {
     }
     
   }
-  
 }
 
 // Compute the distance between 2 points using Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula)
 float getTravelledDistance(float aux_lat, float aux_lon) {
-  
+  // Computation of the error based on a normal distribution
+  float err = gen.random(); // Error
+  if (err < 0) err = -err;
+
+  // Computation of the distance using the Haversine formula
   float fi = toRadians(aux_lat - f_lat);
   float lambda = toRadians(aux_lon - f_lon);
   float a = pow(sin(fi/2),2) + cos(toRadians(f_lat)) * cos(toRadians(aux_lat)) * pow(sin(lambda/2),2);
   float d = 2 * EARTH_RADIUS * asin(sqrt(a));
+
+  // Printings to debug the code
   Serial.print("\nDistance between (");
   Serial.print(f_lat,8); Serial.print(", "); Serial.print(f_lon,8); Serial.print(") & (");
   Serial.print(aux_lat,8); Serial.print(", "); Serial.print(aux_lon,8); Serial.print("): ");
   Serial.print(d, 8); Serial.print("-"); Serial.print(err); Serial.print(" = ");
   d -= err; Serial.print(d, 8); Serial.println("m");
-  
+
+  // Uncomment when deleting the printings
+  // d -= err; 
   return d;
 }
 
+// Computes the given degrees angle in radians
 float toRadians(float degr) {
   return degr * PI/180;
 }
 
+// Parses the GNSS coordinates given by the antenna (hhmm.mmmm) into hours
 float parseGNSS(float coord) {
   float aux;
   aux = trunc(coord/100) + (coord - trunc(coord/100)*100)/60;
   return aux;
 }
 
+// Prints the content of a char array
 void printCharArr(char arr[]) {
   int len = strlen(arr);
   for (int i = 0; i < len; i++) {
