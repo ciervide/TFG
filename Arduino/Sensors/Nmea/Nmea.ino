04f9cb0063f4e371897386ae12fbe52ecf8b12ae
ciervide <ciervide@users.noreply.github.com>
2020-06-24 18:59:48 +0200

    Update Nmea.ino
    Functionality of reading speed added and code reorganized

--- Arduino/Sensors/Nmea/Nmea.ino
+++ Arduino/Sensors/Nmea/Nmea.ino
@@ -6,6 +6,7 @@
 
 SoftwareSerial gps = SoftwareSerial(rxPin, txPin);
 String str = "";
+String GLL = "", VTG = "";
 float f_lat = 180, f_lon = 180, dist = 0, max_dist = 0, num_measures = 0;
 float EARTH_RADIUS = 6371e3;
 
@@ -39,40 +40,16 @@ void loop() {
       inc = gps.read();
       if (inc == '$') {
         if (str.indexOf("$GNGLL") >= 0) {
-          // Needed variables to extract the coordinates
-          char arr[str.length()];
-          str.toCharArray(arr, str.length() + 1);
-          float aux_lat, aux_lon;
-  
-          // Read coordinates
-          char *token; token = strtok(arr, ",");
-          token = strtok(NULL, ","); aux_lat = parseGNSS(atof(token)); token = strtok(NULL, ",");
-          if (token[0] == 'S') aux_lat = -aux_lat; // If latitude is on the South, convert it to negative
-          if ((aux_lat < -90) || (aux_lat > 90)) aux_lat = 0;
-          token = strtok(NULL, ","); aux_lon = parseGNSS(atof(token)); token = strtok(NULL, ",");
-          if (token[0] == 'W') aux_lon = -aux_lon; // If longitude is on the West, convert it to negative
-          if ((aux_lon < -180) || (aux_lon > 180)) aux_lon = 0;
-          
-          // Compute distance between stored and new coordinates
-          if ((f_lat == 180) && (f_lon == 180)) {
-            if ((aux_lat != 0) && (aux_lon != 0)) {
-              f_lat = aux_lat; f_lon = aux_lon;
-              // Notify error
-            }
-          } else {
-            dist += getTravelledDistance(aux_lat, aux_lon);
-            f_lat = aux_lat; f_lon = aux_lon;
-            Serial.print("Travelled distance: "); Serial.print(dist, 4); Serial.println("m");
-          }
-          
-          // To increase sampling time
-          //previousMillis = currentMillis;
-          
+          GLL = str;
+        } else if (str.indexOf("$GNVTG") >= 0) {
+          VTG = str;
         }
         str = "$";
       } else {
         str += inc;
       }
+      if ((GLL != "") && (VTG != ""))
+        extractFromNMEA();
     }
     
   }
@@ -120,4 +97,46 @@ void printCharArr(char arr[]) {
   for (int i = 0; i < len; i++) {
     Serial.print(arr[i]);
   }
+}
+
+// Function that extract the desired data from the NMEA strings
+void extractFromNMEA() {
+  // Needed variables to extract the coordinates
+  char arrGLL[GLL.length()];
+  GLL.toCharArray(arrGLL, GLL.length() + 1);
+  float aux_lat, aux_lon;
+
+  // Read coordinates
+  char *token; token = strtok(arrGLL, ",");
+  token = strtok(NULL, ","); aux_lat = parseGNSS(atof(token)); token = strtok(NULL, ",");
+  if (token[0] == 'S') aux_lat = -aux_lat; // If latitude is on the South, convert it to negative
+  if ((aux_lat < -90) || (aux_lat > 90)) aux_lat = 0;
+  token = strtok(NULL, ","); aux_lon = parseGNSS(atof(token)); token = strtok(NULL, ",");
+  if (token[0] == 'W') aux_lon = -aux_lon; // If longitude is on the West, convert it to negative
+  if ((aux_lon < -180) || (aux_lon > 180)) aux_lon = 0;
+  
+  // Compute distance between stored and new coordinates
+  if ((f_lat == 180) && (f_lon == 180)) {
+    if ((aux_lat != 0) && (aux_lon != 0)) {
+      f_lat = aux_lat; f_lon = aux_lon;
+      // Notify error
+    }
+  } else {
+    dist += getTravelledDistance(aux_lat, aux_lon);
+    f_lat = aux_lat; f_lon = aux_lon;
+    Serial.print("Travelled distance: "); Serial.print(dist, 4); Serial.println("m");
+  }
+
+  // Needed variables to extract the speed
+  char arrVTG[VTG.length()];
+  VTG.toCharArray(arrVTG, VTG.length() + 1);
+
+  // Read speed
+  token = strtok(arrVTG, ","); token = strtok(NULL, ",");
+  token = strtok(NULL, ","); token = strtok(NULL, ",");
+  token = strtok(NULL, ","); token = strtok(NULL, ",");
+  Serial.print("Actual speed: "); Serial.println(atof(token));
+
+  // Clean GLL and VTG
+  GLL = ""; VTG = "";
 }
